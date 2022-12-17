a13486d59b2aca4eb686624dced35283875171da
ret7020 <ret7020@gmail.com>
2022-12-17 21:50:00 +0300

    Audio loopback

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Typography, Input } from 'antd';
+import "./App.css";
+
+const { Title, Text } = Typography;
+const { Search } = Input;
 
 function App() {
+  function handleSuccess(stream) {
+    const audio = document.getElementById("base");
+      audio.controls = true;
+      audio.autoplay = true;
+      window.stream = stream;
+      audio.srcObject = stream;
+      stream.oninactive = function () {
+        console.log("Stream ended");}
+  }
+
+  useEffect(() => {
+    if (navigator.mediaDevices) {
+      const constraints = (window.constraints = {
+        audio: true,
+        video: false,
+      });
+      navigator.mediaDevices.getUserMedia(constraints).then((stm) => {
+        handleSuccess(stm);
+      });
+    }
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title code>Karaoke by @ret7020</Title>
+      <Text code>ReactJS + AntDesign + Gh Page</Text>
+      <Text code>Source on GitHub: <Text strong>ret7020/SchoolKaraoke</Text></Text>
+      <Search className="search" placeholder="Search karaoke version" enterButton addonBefore="караоке"/>
+      
+      <audio id="base" style={{display: "none"}}></audio>
     </div>
   );
 }
